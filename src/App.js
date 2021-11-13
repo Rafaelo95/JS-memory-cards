2b08de6eb422dcafaf93f75f0a209e5a5daa3656
Rafaelo Antonov <ralf_antonov@abv.bg>
2021-11-13 10:44:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,6 +29,17 @@ function App() {
     <div className="App">
         <h1>Magic Match</h1>
         <button onClick={shuffleCards}>New Game</button>
+
+        <div className="card-grid">
+          {cards.map(card => (
+            <div className="card" key={card.id}>
+              <div>
+                <img className="front" src={card.src} alt="card" />
+                <img className="back" src="../img/cover.png" alt="cover" />
+              </div>
+            </div>
+          ))}
+        </div>
     </div>
   );
 }
