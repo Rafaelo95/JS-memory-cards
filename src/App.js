e3d2f059be0f7d2c9d943954acd14c13ad0f4ba2
Rafaelo Antonov <ralf_antonov@abv.bg>
2021-11-13 10:20:20 +0200

    Update App.js
    created shuffleCards function to double the amount of images and randomize their order

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import './App.css';
 
-const cards = [
+const cardImages = [
   {"src": "/img/helmet-1.png"},
   {"src": "/img/potion-1.png"},
   {"src": "/img/ring-1.png"},
@@ -10,11 +11,24 @@ const cards = [
 ]
 
 function App() {
-  
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random()}))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
         <h1>Magic Match</h1>
-        <button>New Game</button>
+        <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
